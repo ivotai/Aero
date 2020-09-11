12a71a8fde97cdc91c5b4ec001b362a6b7e033dd
ivotai <ivolianer2015@163.com>
2020-09-11 08:51:24 +0800

    2020-9-10

--- app/src/main/java/com/sanre/ui/LoginAct.kt
+++ app/src/main/java/com/sanre/ui/LoginAct.kt
@@ -1,30 +1,28 @@
 package com.sanre.ui
 
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.orhanobut.logger.Logger
-import com.rxjava.rxlife.life
 import com.sanre.R
 import com.sanre.app.di.ComponentHolder
 import com.sanre.data.api.SimpleApi
-import retrofit2.Retrofit
-import javax.inject.Inject
+import com.tbruyelle.rxpermissions3.RxPermissions
 
 class LoginAct : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        login()
+//        login()
+//        requestPer()
     }
 
-    lateinit var simpleApi:SimpleApi
+    lateinit var simpleApi: SimpleApi
 
     private fun login() {
         val username = "13611840424"
         val password = "111111"
-        Logger.e(ComponentHolder.appComponent.api().toString())
-        Logger.e(ComponentHolder.appComponent.api().toString())
 
 //        simpleApi.login(username = username, password = password)
 //            .subscribeOn(Schedulers.io())
@@ -41,6 +39,18 @@ class LoginAct : AppCompatActivity() {
 //                override fun onError(e: Throwable?) {
 //                }
 //            })
+    }
+
+    private fun requestPer() {
+        RxPermissions(this)
+            .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .subscribe { granted ->
+                if (granted) { // Always true pre-M
+                    // I can control the camera now
+                } else {
+                    finish()
+                }
+            }
     }
 
 }
